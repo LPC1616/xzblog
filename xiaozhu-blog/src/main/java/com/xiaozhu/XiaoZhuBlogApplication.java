5281645113692cd25d5946f41b782833f96ce59d
LPC1616 <2249427154@qq.com>
2023-02-13 15:20:45 +0800

    使用swagger生成接口文档

--- xiaozhu-blog/src/main/java/com/xiaozhu/XiaoZhuBlogApplication.java
+++ xiaozhu-blog/src/main/java/com/xiaozhu/XiaoZhuBlogApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.xiaozhu.mapper")
 @EnableScheduling
+@EnableSwagger2
 public class XiaoZhuBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(XiaoZhuBlogApplication.class, args);
--- xiaozhu-blog/src/main/java/com/xiaozhu/controller/CommentController.java
+++ xiaozhu-blog/src/main/java/com/xiaozhu/controller/CommentController.java
@@ -2,13 +2,20 @@ package com.xiaozhu.controller;
 
 import com.xiaozhu.constants.SystemConstants;
 import com.xiaozhu.domain.ResponseResult;
+import com.xiaozhu.domain.dto.AddCommentDto;
 import com.xiaozhu.domain.entity.Comment;
 import com.xiaozhu.service.CommentService;
+import com.xiaozhu.utils.BeanCopyUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/comment")
+@Api(tags = "评论",description = "评论相关接口")
 public class CommentController {
 
     @Autowired
@@ -20,11 +27,17 @@ public class CommentController {
     }
 
     @PostMapping
-    public ResponseResult addComment(@RequestBody Comment comment){
+    public ResponseResult addComment(@RequestBody AddCommentDto addCommentDto){
+        Comment comment = BeanCopyUtils.copyBean(addCommentDto, Comment.class);
         return commentService.addComment(comment);
     }
 
+    @ApiOperation(value = "友链评论列表",notes = "获取一页友链评论")
     @GetMapping("/linkCommentList")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum",value = "页号"),
+            @ApiImplicitParam(name = "pageSize",value = "每页大小")
+    })
     public ResponseResult linkCommentList(Integer pageNum,Integer pageSize){
         return commentService.commentList(SystemConstants.LINK_COMMENT,null,pageNum,pageSize);
     }
