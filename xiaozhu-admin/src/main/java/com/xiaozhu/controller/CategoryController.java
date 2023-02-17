b04cee171b2c7c5dc6e4524088c65f01fa250e0b
LPC1616 <2249427154@qq.com>
2023-02-17 22:19:36 +0800

    后台分类名导出

--- xiaozhu-admin/src/main/java/com/xiaozhu/controller/CategoryController.java
+++ xiaozhu-admin/src/main/java/com/xiaozhu/controller/CategoryController.java
@@ -1,13 +1,21 @@
 package com.xiaozhu.controller;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson.JSON;
 import com.xiaozhu.domain.ResponseResult;
+import com.xiaozhu.domain.entity.Category;
 import com.xiaozhu.domain.vo.CategoryVo;
+import com.xiaozhu.domain.vo.ExcelCategoryVo;
+import com.xiaozhu.enums.AppHttpCodeEnum;
 import com.xiaozhu.service.CategoryService;
+import com.xiaozhu.utils.BeanCopyUtils;
+import com.xiaozhu.utils.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -22,5 +30,23 @@ public class CategoryController {
         return ResponseResult.okResult(list);
     }
 
-    
+    @GetMapping("/export")
+    public void export(HttpServletResponse response){
+        try {
+            //设置下载文件的请求头
+            WebUtils.setDownLoadHeader("分类.xlsx",response);
+            //获取需要导出的数据
+            List<Category> categoryVos = categoryService.list();
+
+            List<ExcelCategoryVo> excelCategoryVos = BeanCopyUtils.copyBeanList(categoryVos, ExcelCategoryVo.class);
+            //把数据写入到Excel中
+            EasyExcel.write(response.getOutputStream(), ExcelCategoryVo.class).autoCloseStream(Boolean.FALSE).sheet("分类导出")
+                    .doWrite(excelCategoryVos);
+
+        } catch (Exception e) {
+            //如果出现异常也要响应json
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR);
+            WebUtils.renderString(response, JSON.toJSONString(result));
+        }
+    }
 }
---
+++ xiaozhu-framework/src/main/java/com/xiaozhu/domain/vo/ExcelCategoryVo.java
@@ -0,0 +1,21 @@
+package com.xiaozhu.domain.vo;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExcelCategoryVo {
+
+    @ExcelProperty("分类名")
+    private String name;
+
+    @ExcelProperty("描述")
+    private String description;
+
+    @ExcelProperty("状态 0:正常,1禁用")
+    private String status;
+}
